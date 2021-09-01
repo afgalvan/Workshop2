6ea83cb1a0829ebf1333d0e40033d90a074a66c0
Andres <afgalvan@unicesar.edu.co>
2021-09-01 23:11:49 +0100

    chore: add generic repository interfaces

---
+++ src/Domain/Shared/IMutableRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain.Shared
+{
+    public interface IMutableRepository<in TId, TEntity> : IImmutableRepository<TId, TEntity>
+    {
+        public Task RemoveById(TId id, CancellationToken cancellation);
+        public Task UpdateById(TId id, TEntity newData, CancellationToken cancellation);
+    }
+}
