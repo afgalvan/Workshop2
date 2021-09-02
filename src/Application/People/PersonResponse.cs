0a091c59d8eaf991e6658ea6f63fbf1590502aae
Andres <afgalvan@unicesar.edu.co>
2021-09-02 15:37:53 +0100

    chore: add person response entity

---
+++ src/Application/People/PersonResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.People
+{
+    public class PersonResponse
+    {
+        public string Id    { get; init; }
+        public string Name  { get; init; }
+        public string Genre { get; init; }
+        public int    Age   { get; init; }
+
+        public PersonResponse(string id, string name, int age, string genre)
+        {
+            Id    = id;
+            Name  = name;
+            Age   = age;
+            Genre = genre;
+        }
+    }
+}
